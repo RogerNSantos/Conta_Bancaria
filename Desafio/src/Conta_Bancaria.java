f173740c82a1b8ef1b934608affc6043d5ff4408
Rogério Neves dos Santos <103578702+RogerNSantos@users.noreply.github.com>
2023-11-17 16:58:18 -0300

    Add files via upload

--- Desafio/src/Conta_Bancaria.java
+++ Desafio/src/Conta_Bancaria.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
-
 public class Conta_Bancaria {
     public static void main(String[] args) {
         String nome = "Rogério Santos";
         String tipoConta = "Corrente";
         double saldoConta = 2278.65;
-        int opcao = 0;
 
         System.out.println("\n====================================");
         System.out.println("* * * Extrato Conta Corrente * * * *");
@@ -13,23 +10,6 @@ public class Conta_Bancaria {
         System.out.println("\nNome do Cliente: " + nome);
         System.out.println("Tipo de conta: " + tipoConta);
         System.out.println("Saldo atualizado: " + saldoConta);
-
-        String menu = """
-                == Escolha uma opção: 
-                1 - Consultar saldo: 
-                2 - Fazer tranferência:
-                3 - Receber:
-                4 - Sair:
-                                 
-                """;
-
-        Scanner leitura = new Scanner(System.in);
-
-        while (opcao !=4) {
-            System.out.println(menu);
-            opcao = leitura.nextInt();
-
-        }
-
     }
+
 }
