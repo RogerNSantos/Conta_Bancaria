b56732a8a5129765c1dff27393c3773865ec5ad8
Rogério Neves dos Santos <103578702+RogerNSantos@users.noreply.github.com>
2023-11-17 16:57:56 -0300

    Add files via upload

---
+++ Conta_Bancaria.java
@@ -0,0 +1,15 @@
+public class Conta_Bancaria {
+    public static void main(String[] args) {
+        String nome = "Rogério Santos";
+        String tipoConta = "Corrente";
+        double saldoConta = 2278.65;
+
+        System.out.println("\n====================================");
+        System.out.println("* * * Extrato Conta Corrente * * * *");
+        System.out.println("====================================");
+        System.out.println("\nNome do Cliente: " + nome);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo atualizado: " + saldoConta);
+    }
+
+}
